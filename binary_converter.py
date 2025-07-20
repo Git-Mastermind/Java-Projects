bb1fcaf8e9eedbe85b184d9d6dc7b24fbe602476
Eshan Jha <eshan-jha@outlook.com>
2025-07-20 11:29:04 -0700

    create new file called binary converter

---
+++ binary_converter.py
@@ -0,0 +1,13 @@
+def deci_to_binary(number):
+    binary_answer = []
+    if number == 0:
+        print(0)
+    else:
+        while number > 0:
+            binary_digit = number % 2
+            binary_answer.append(binary_digit)
+            number = number // 2
+
+        print(*binary_answer[::-1])
+
+deci_to_binary(int(input('Enter a number: ')))
