46a934ce623a5791afc173c6ab27a24f77361d6c
Eshan Jha <eshan-jha@outlook.com>
2025-07-24 16:58:24 -0700

    make new file called factorial_calculator and modify money_tracker

--- money_tracker.py
+++ money_tracker.py
@@ -10,21 +10,17 @@ def withdraw(balance, amount):
 def check_balance(balance):
     print(f'Your balance is: 💲{balance}')
 
-while balance > 0:
-    bank_selection = ''
-    while bank_selection != 'q':
-        bank_selection = input('(D)eposit, (W)ithdraw, or (C)heck Balance? (q to quit): ').lower()
-        if bank_selection == 'd':
-            amount = int(input('Deposit amount: '))   
-            balance = deposit(balance, amount)
-        elif bank_selection == 'w':
-            amount = int(input('Withdraw amount: '))
-            balance = withdraw(balance, amount)
-        elif bank_selection == 'c':
-            check_balance(balance)
-
-    else:
-        quit()
+bank_selection = ''
+while bank_selection != 'q':
+    bank_selection = input('(D)eposit, (W)ithdraw, or (C)heck Balance? (q to quit): ').lower()
+    if bank_selection == 'd':
+        amount = int(input('Deposit amount: '))   
+        balance = deposit(balance, amount)
+    elif bank_selection == 'w':
+        amount = int(input('Withdraw amount: '))
+        balance = withdraw(balance, amount)
+    elif bank_selection == 'c':
+        check_balance(balance)
 
 else:
-    print('Your balance is less than zero!')
+    quit()
