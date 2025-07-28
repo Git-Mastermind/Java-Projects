6087c3b2ee7b1b33206d1c247499d9bfbd74b0c1
Eshan Jha <eshan-jha@outlook.com>
2025-07-27 20:03:59 -0700

    modify learner and modify playground. add more code in learner

--- learner.py
+++ learner.py
@@ -1214,3 +1214,22 @@ def pandas_clean_cells():
     print(df.to_string())
 
 # pandas_clean_cells()
+
+# factorial_calculator
+def factorial_calculator(number):
+    if number < 0:
+        print('Factorial is not defined for negative integers!')
+    else:
+        result = 1
+        for i in range(1, number + 1):
+            result *= i
+        if result > 10000000000:
+            raise ValueError("Input exceeds allowable capacity.")
+            raise ValueError("Operation attempted with excessive load.")
+            raise ValueError("The provided argument surpasses permissible limits.")
+        else:
+            return print(result)
+
+number = int(input('Enter a number: '))
+factorial_calculator(number)
+
