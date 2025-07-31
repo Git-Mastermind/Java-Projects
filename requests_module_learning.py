76c5eca53c57ee47e52a17aee0c52a886ebd9bfc
Eshan Jha <eshan-jha@outlook.com>
2025-07-31 10:20:50 -0700

    modify requests_module_learning

--- requests_module_learning.py
+++ requests_module_learning.py
@@ -40,3 +40,72 @@ def motivational_quote_getter():
 
 
 # motivational_quote_getter()
+
+def post_blog_data():
+    url = 'https://jsonplaceholder.typicode.com/posts'
+    data = {
+        'title':"Eshan's Coding Blog",
+        'body':'I coded today, and it felt magical!',
+        'userId':13
+    }
+
+    post = requests.post(url, json=data)
+
+    if post.status_code == 201:
+        print('✅Everything went well!')
+    else:
+        print('❌Something went wrong!')
+# post_blog_data()
+
+def topic_related_jokes():
+    url_categories = {
+        'general':'https://official-joke-api.appspot.com/jokes/general/random',
+        'programming':'https://official-joke-api.appspot.com/jokes/programming/random',
+        'knock-knock':'https://official-joke-api.appspot.com/jokes/knock-knock/random'
+    }
+    type_of_joke = input('Enter the type of joke (general, programming or knock-knock): ').lower()
+    if type_of_joke == 'general':
+        data = requests.get(url_categories['general']).json()
+        joke = data[0]['setup']
+        punchline = data[0]['punchline']
+        print(joke)
+        time.sleep(2)
+        print(punchline)
+
+    elif type_of_joke == 'programming':
+        data = requests.get(url_categories['programming']).json()
+        joke = data[0]['setup']
+        punchline = data[0]['punchline']
+        print(joke)
+        time.sleep(2)
+        print(punchline)
+    
+    elif type_of_joke == 'knock-knock' or type_of_joke == 'knock knock':
+        data = requests.get(url_categories['knock-knock']).json()
+        joke = data[0]['setup']
+        punchline = data[0]['punchline']
+        print(joke)
+        time.sleep(2)
+        print(punchline)
+
+
+# topic_related_jokes()
+
+def topic_related_jokes_cleanedup():
+    url_categories = {
+        'general':'https://official-joke-api.appspot.com/jokes/general/random',
+        'programming':'https://official-joke-api.appspot.com/jokes/programming/random',
+        'knock-knock':'https://official-joke-api.appspot.com/jokes/knock-knock/random'
+    }
+    type_of_joke = input('Enter the type of joke (general, programming, or knock-knock): ').lower()
+    if ty
+    if type_of_joke == 'knock knock':
+        type_of_joke = 'knock-knock'
+    if type_of_joke in url_categories:
+        data = requests.get(url_categories[type_of_joke]).json()
+        joke = data[0]['setup']
+        punchline = data[0]['punchline']
+        print(joke)
+        time.sleep(2)
+        print(punchline)
+# topic_related_jokes_cleanedup()
