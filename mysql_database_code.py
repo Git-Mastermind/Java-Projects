4e6d0d13bc82618fda6c99312921ae520961dff2
Eshan Jha <eshan-jha@outlook.com>
2025-08-17 11:57:45 -0700

    modify mysql_database_code

--- mysql_database_code.py
+++ mysql_database_code.py
@@ -11,48 +11,45 @@ connection = mysql.connector.connect(
 print('Connected to MySQL!')
 
 cursor = connection.cursor()
+def top_bottom_3_cities():
+    # First query
+    cursor.execute("SELECT city, AVG(rent_amount) AS avg_rent FROM apartments GROUP BY city ORDER BY avg_rent DESC LIMIT 3;")
+    top_3_cities = cursor.fetchall()  # fetch before running next query
+    city_names = ''
+    loop_counter = -1
+    for city in top_3_cities:
+        loop_counter += 1
+        if loop_counter == 0:
+            city_names = city[0]
+        elif 1 <= loop_counter < len(top_3_cities) - 1:
+            city_names = city_names + ', ' + city[0]
+        else:
+            city_names = city_names + ' and ' + city[0] + '.'
 
-# First query
-cursor.execute("SELECT city, AVG(rent_amount) AS avg_rent FROM apartments GROUP BY city ORDER BY avg_rent DESC LIMIT 3;")
-top_3_cities = cursor.fetchall()  # fetch before running next query
-city_names = ''
-loop_counter = -1
-for city in top_3_cities:
-    loop_counter += 1
-    if loop_counter == 0:
-        city_names = city[0]
-    elif 1 <= loop_counter < len(top_3_cities) - 1:
-        city_names = city_names + ', ' + city[0]
-    else:
-        city_names = city_names + ' and ' + city[0]
+    print(f'The top 3 cities are {city_names}')
 
-print(f'The top 3 cities are {city_names}')
+    # Line break
+    print('')
 
+    # Second query
+    cursor.execute("SELECT city, AVG(rent_amount) AS avg_rent FROM apartments GROUP BY city ORDER BY avg_rent LIMIT 3;")
+    bottom_3_cities = cursor.fetchall()
+    top_3_cities = cursor.fetchall()  # fetch before running next query
+    bottom_cities = ''
+    loop_counter = -1
+    for city in bottom_3_cities:
+        loop_counter += 1
+        if loop_counter == 0:
+            bottom_cities  = city[0]
+        elif 1 <= loop_counter < len(bottom_3_cities) - 1:
+            bottom_cities = bottom_cities + ', ' + city[0]
+        else:
+            bottom_cities = bottom_cities + ' and ' + city[0] + '.'
 
+    print(f'The bottom 3 cities are {bottom_cities}')
 
+    # Close connection
+    cursor.close()
+    connection.close()
 
-
-
-# Line break
-print('')
-
-# Second query
-cursor.execute("SELECT city, AVG(rent_amount) AS avg_rent FROM apartments GROUP BY city ORDER BY avg_rent LIMIT 3;")
-bottom_3_cities = cursor.fetchall()
-top_3_cities = cursor.fetchall()  # fetch before running next query
-bottom_cities = ''
-loop_counter = -1
-for city in bottom_3_cities:
-    loop_counter += 1
-    if loop_counter == 0:
-        bottom_cities  = city[0]
-    elif 1 <= loop_counter < len(bottom_3_cities) - 1:
-        bottom_cities = bottom_cities + ', ' + city[0]
-    else:
-        bottom_cities = bottom_cities + ' and ' + city[0]
-
-print(f'The bottom 3 cities are {bottom_cities}')
-
-# Close connection
-cursor.close()
-connection.close()
+top_bottom_3_cities()
