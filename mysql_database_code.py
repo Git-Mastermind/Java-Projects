47ffd2458289c5f0da22649e9dc8ea76e40d8f51
Eshan Jha <eshan-jha@outlook.com>
2025-08-16 18:10:56 -0700

    modify mysql_database_code

--- mysql_database_code.py
+++ mysql_database_code.py
@@ -5,13 +5,13 @@ connection = mysql.connector.connect(
     host="localhost",       # or your server IP
     user="eshanjha",            # your MySQL username
     password="ILovebooks!@#123",# your MySQL root password
-    database="sql_invoicing"        # the DB you want to use
+    database="enu_db"        # the DB you want to use
 )
 
 cursor = connection.cursor()
 
 # Example query
-cursor.execute("SELECT name AS client, AVG(amount) FROM clients c JOIN payments p USING (client_id) GROUP BY client")
+cursor.execute("SELECT first_name FROM renters WHERE first_name LIKE 'J%'")
 
 # Fetch and print results
 for row in cursor.fetchall():
