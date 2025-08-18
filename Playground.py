c68139a666e51351969b260e3f289139f1f6e3d4
Eshan Jha <eshan-jha@outlook.com>
2025-08-18 11:01:14 -0700

    modify playground

--- Playground.py
+++ Playground.py
@@ -13,12 +13,27 @@
 
 # print(city_names)
     
-input_sentence = input('Enter a sentence (each word space seperated): ')
-each_word = input_sentence.split()
-first_letter_of_word = []
-for i in range(len(each_word)):
-    first_letter_of_word.append(each_word[i][0])
-print(*first_letter_of_word)
+def first_letter_of_words():
+    input_sentence = input('Enter a sentence (each word space seperated): ')
+    each_word = input_sentence.split()
+    first_letter_of_word = []
+    for i in range(len(each_word)):
+        first_letter_of_word.append(each_word[i][0])
+    print(*first_letter_of_word)
+
+
+def first_letter_of_each_word():
+    input_sentence = input('Enter a sentence (seperated by space) ')
+    characters = list(input_sentence)
+    result = []
+    for i in range(len(characters)):
+        if i == 0:
+            result.append(characters[i])
+        elif characters[i] == ' ':
+            result.append(characters[i + 1])
+    print(*result)
+
+first_letter_of_each_word()
 
 
 
