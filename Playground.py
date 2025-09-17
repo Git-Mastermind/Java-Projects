ff60f5d8d2d6aa2307cbface63d73887c1186a03
Eshan Jha <eshan-jha@outlook.com>
2025-09-17 15:31:10 -0700

    modify Playground and create new file called learn_java.java

--- Playground.py
+++ Playground.py
@@ -1,7 +1,11 @@
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as mat
+from flask import Flask
 
-data = pd.read_csv('sales.csv')
-avg_burgers = np.mean(data['Burgers'])
-print(f'Avg Burgers sold: {avg_burgers}')
+app = Flask(__name__)
+
+@app.route('/hello_world')
+
+def hello():
+    return print('Hello World')
+
+if __name__ == '__main__':
+    app.run(debug=True)
