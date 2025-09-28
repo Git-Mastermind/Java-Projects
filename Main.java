40544550a03a280b55f9b16070f40b1db9ab5946
Eshan Jha <eshan-jha@outlook.com>
2025-09-27 19:43:03 -0700

    modify and update Main.java, complete advanced mortgage calculater

--- Main.java
+++ Main.java
@@ -4,24 +4,23 @@ import java.awt.*;
 import java.text.NumberFormat;
 import java.util.Scanner;
 
+
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Temp: ");
-        int temperature = scanner.nextInt();
-        if (temperature > 65) {
-            System.out.println("It's a hot day! Drink plenty of water");
-        }
-    else if (temperature > 20 && temperature < 30) {
-        System.out.println("It's a nice day");
-    }
-    else {
-        System.out.println("It's cold");
-    }
-
-    
+        
 
 
+
+        
+         
+
+
+
+
+        
+        
+
+            
 
 
 
@@ -51,9 +50,71 @@ public class Main {
 
 
 
+
+// Projects
 
 
 }
+    public void advancedMortgageCalculater() {
+        Scanner scanner = new Scanner(System.in);
+        final byte MONTHS_IN_YEAR = 12;
+        final byte PERCENT  = 100;
+
+        int principal = 0;
+        double annualInterestRate = 0;
+        int periodInYears = 0;
+
+        while (true) {
+            System.out.print("Principal ($1K - $1M): ");
+            principal = scanner.nextInt();
+            if (principal >= 1_000 && principal <= 1_000_000) {
+                break;
+            }
+            else {
+                System.out.println("Enter a number between 1,000 and 1,000,000");
+            }
+        }
+
+        while (true) {
+            System.out.print("Annual Interest Rate: ");
+            annualInterestRate = scanner.nextFloat();
+            if (annualInterestRate <= 100) {
+                break;
+            }
+            else {
+                System.out.println("Enter a valid interest rate (1% - 100%): ");
+            }
+        }
+        double monthlyInterest = annualInterestRate / MONTHS_IN_YEAR;
+        double monthlyInterestRate = monthlyInterest / PERCENT;
+
+        while (true) {
+        System.out.print("Period (Years): ");
+        periodInYears = scanner.nextInt();
+        if (periodInYears >= 10 && periodInYears <= 30) {
+            break;
+        }
+        else {
+            System.out.println("Please enter a valid time frame (10 - 30 years)");
+        }
+        }
+
+        double periodInMonths = periodInYears * MONTHS_IN_YEAR;
+
+        double numerator = (principal) * (monthlyInterestRate * Math.pow(1 + monthlyInterestRate, periodInMonths));
+
+        double denominator = Math.pow(1 + monthlyInterestRate, periodInMonths) - 1;
+
+        double mortgageCalculation = (numerator / denominator);
+
+        NumberFormat currency = NumberFormat.getCurrencyInstance();
+        String mortgage = currency.format(mortgageCalculation);
+        System.out.println(mortgage);
+
+
+
+        }
+            
 
     public void mortgageCalculater() {
         Scanner scanner = new Scanner(System.in);
@@ -81,5 +142,39 @@ public class Main {
 
 
     }
+
+    public void tipCalculater() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Total: ");
+        double totalBill = scanner.nextFloat();
+
+        System.out.print("Tip (percentage): ");
+        double tipPercent = scanner.nextFloat();
+
+        double tipAmount = (totalBill * tipPercent) / 100;
+        double totalAmount = totalBill + tipAmount;
+        NumberFormat currency = NumberFormat.getCurrencyInstance();
+
+        String finalBill = currency.format(totalAmount);
+        System.out.println(finalBill);
+    }
+
+    public void fizzBuzz() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Number: ");
+        int number = scanner.nextInt();
+
+        if (number % 5 == 0 && number % 3 == 0)
+            System.out.println("FizzBuzz"); 
+        else if (number % 5 == 0) 
+            System.out.println("Fizz");
+        else if (number % 3 == 0)
+            System.out.println("Buzz");
+        else
+            System.out.println(number);
+    }
+
+
+    
 } 
 
