654dc3d3bf4797af7086df70423e6f44cf4ae8af
Eshan Jha <eshan-jha@outlook.com>
2025-10-01 19:48:48 -0700

    create numberGuesser game, add to Games class

--- Java-Projects/Main.java
+++ Java-Projects/Main.java
@@ -9,11 +9,35 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        String text = "     Hello World     ";
-        System.out.println("Before: [" + text + "]");
-        System.out.println("After: [" + text.trim() + "]");
+        Scanner scanner = new Scanner(System.in);
+        int randomNumber = (int) (Math.random() * 101);
+        int numberOfGuesses = 7;
+        while (true) {
+            System.out.print("Guess (" + numberOfGuesses + "): ");
+            int numberGuess = scanner.nextInt();
 
+            if (numberOfGuesses == 1) {
+                System.out.println("You ran out of guesses! The number was " + randomNumber);
+                break;
+            }
+            else if (numberGuess > 100 || numberGuess < 1) {
+                System.out.println("Invalid domain (1 - 100)!");
+            }
 
+            else if (numberGuess == randomNumber) {
+                System.out.println("You win! The number was " + randomNumber);
+                break;
+            }
+            else if (numberGuess > randomNumber) {
+                System.out.println("Too high!");
+                numberOfGuesses--;
+            }
+            else if (numberGuess < randomNumber) {
+                System.out.println("Too low!");
+                numberOfGuesses--;
+            }
+        }
+        scanner.close();
         
 
 }
