814e874ac7bc201ef89f5a614f668dce24c75205
Eshan Jha <eshan-jha@outlook.com>
2025-10-01 18:41:16 -0700

    modify Main.java

--- Java-Projects/Main.java
+++ Java-Projects/Main.java
@@ -4,18 +4,17 @@ import java.awt.*;
 import java.nio.channels.InterruptedByTimeoutException;
 import java.text.NumberFormat;
 import java.util.Scanner;
-import java.sql.*;
-
 
 
 
 public class Main {
     public static void main(String[] args) {
-        var x = 6;
-        var y = 6.1;
-        System.out.println(y);
+        String text = "     Hello World     ";
+        System.out.println("Before: [" + text + "]");
+        System.out.println("After: [" + text.trim() + "]");
 
 
+        
 
 }
 }
