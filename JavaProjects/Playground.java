14c79fdb1781902c760a4798cef88dddba1c9355
Eshan Jha <eshan-jha@outlook.com>
2025-10-09 16:38:04 -0700

    modify Playground

--- JavaProjects/Playground.java
+++ JavaProjects/Playground.java
@@ -1,8 +1,12 @@
+
 import java.util.Scanner;
 
 
 public class Playground {
     public static void main(String[] args) {
-        
+       int[] numbers = { 1,2,3,4,5 };
+       for (int num : numbers) {
+        System.out.println(num);
+       } 
     }
 }
