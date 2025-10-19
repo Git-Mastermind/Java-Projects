2bc85e7620073c964cb84a97d0fb7ea894aeb445
Eshan Jha <eshan-jha@outlook.com>
2025-10-18 19:57:18 -0700

    modify Time in BudgetPlanner, still resolving issue in BudgetPlannerFuncs

--- BudgetPlanner/Time.java
+++ BudgetPlanner/Time.java
@@ -1,14 +1,15 @@
 
 
 public class Time {
+    
     public void sleep(int milli) {
-    try {
-        Thread.sleep(milli);
+        try {
+            Thread.sleep(milli);
+        }
+        catch (InterruptedException e) {
+            System.out.println("Error! Sleep was interupted!");
+        }
     }
-    catch (InterruptedException e) {
-        System.out.println("Error! Sleep was interupted!");
-    }
-}
     public void loadPage(int milli) {
         System.out.println("Loading page...");
         try {
@@ -18,12 +19,12 @@ public class Time {
             System.out.println("Error! Sleep was interrupted!");
         }
     }
-
     public void returnToMenu(int seconds) {
         for (int i = seconds; i >= 0; i--) {
             System.out.println("Returning to Menu in " + i);
             this.sleep(1000);
         }
     }
+
 }
 
